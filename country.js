eb496d01aa49b031a790bd81dc1f98feec3838d0
Ben Jones <benjones1864@gmail.com>
2024-06-11 09:33:14 -0400

    consolidated methods, fixed Algeria colors

--- country.js
+++ country.js
@@ -11,11 +11,15 @@ class Country {
         document.querySelector("#Color1").style.backgroundColor = this.colors[0];
         document.querySelector("#Color2").style.backgroundColor = this.colors[1];
         document.querySelector("#Color3").style.backgroundColor = this.colors[2];
+        document.querySelector("#CountryName").innerText = this.name;
+        document.querySelector("#OfficialLanguage").innerText = this.lang;
+        document.querySelector("#HelloWorld").innerText = this.greeting;
+        document.querySelector("#Flag").src = this.flag;
     }
 }
 let usa = new Country("USA", "English", "Hello World!", ["red", "white", "blue"], "https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/800px-Flag_of_the_United_States.svg.png");
 let mexico = new Country("Mexico", "Spanish", "¡Hola mundo!", ["red", "white", "green"], "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Flag_of_Mexico.svg/800px-Flag_of_Mexico.svg.png");
-let algeria = new Country("Algeria", "Arabic and Berber", "مرحبا بالعالم!", ["blue", "yellow", "red"], "https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Flag_of_Algeria.svg/800px-Flag_of_Algeria.svg.png");
+let algeria = new Country("Algeria", "Arabic and Berber", "مرحبا بالعالم!", ["green", "white", "red"], "https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Flag_of_Algeria.svg/800px-Flag_of_Algeria.svg.png");
 let bulgaria = new Country("Bulgaria", "Bulgarian", "Здравей свят!", ["white", "green", "red"], "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Flag_of_Bulgaria.svg/800px-Flag_of_Bulgaria.svg.png");
 let estonia = new Country("Estonia", "Estonian", "Tere, Maailm!", ["blue", "black", "white"], "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Flag_of_Estonia.svg/800px-Flag_of_Estonia.svg.png");
 
@@ -41,14 +45,5 @@ function SwitchCountry() {
             country = estonia;
             break;
     }
-    ChangeColors(country);
-}
-
-function ChangeColors(country)
-{
     country.displayColors();
-    document.querySelector("#CountryName").innerText = country.name;
-    document.querySelector("#OfficialLanguage").innerText = country.lang;
-    document.querySelector("#HelloWorld").innerText = country.greeting;
-    document.querySelector("#Flag").src = country.flag;
 }
